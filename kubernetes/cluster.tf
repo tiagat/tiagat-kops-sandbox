283ed607c3bfff6606adbdeb0a9758a93ec5ef81
Ruslan Sakal <ruslan.sakal@gmail.com>
2023-10-09 21:17:49 +0200

    control commit

--- kubernetes/cluster.tf
+++ kubernetes/cluster.tf
@@ -1,73 +1,73 @@
-resource "kops_cluster" "cluster" {
+# resource "kops_cluster" "cluster" {
 
-  name               = "cluster.${var.dns_zone_name}"
-  admin_ssh_key      = var.admin_ssh_key
-  kubernetes_version = "1.28"
-  dns_zone           = var.dns_zone_name
-  network_id         = var.vpc_id
+#   name               = "cluster.${var.dns_zone_name}"
+#   admin_ssh_key      = var.admin_ssh_key
+#   kubernetes_version = "1.28"
+#   dns_zone           = var.dns_zone_name
+#   network_id         = var.vpc_id
 
-  cloud_provider {
-    aws {}
-  }
+#   cloud_provider {
+#     aws {}
+#   }
 
-  iam {
-    allow_container_registry = true
-  }
+#   iam {
+#     allow_container_registry = true
+#   }
 
-  networking {
-    calico {}
-  }
+#   networking {
+#     calico {}
+#   }
 
-  topology {
-    masters = "private"
-    nodes   = "private"
-    dns {
-      type = "Public"
-    }
-  }
+#   topology {
+#     masters = "private"
+#     nodes   = "private"
+#     dns {
+#       type = "Public"
+#     }
+#   }
 
-  dynamic "subnet" {
-    for_each = { for subnet in var.private_subnets : subnet.id => subnet }
-    content {
-      type        = "Private"
-      name        = "private-subnet-${subnet.value.index + 1}"
-      provider_id = subnet.value.id
-      zone        = subnet.value.zone
-    }
-  }
+#   dynamic "subnet" {
+#     for_each = { for subnet in var.private_subnets : subnet.id => subnet }
+#     content {
+#       type        = "Private"
+#       name        = "private-subnet-${subnet.value.index + 1}"
+#       provider_id = subnet.value.id
+#       zone        = subnet.value.zone
+#     }
+#   }
 
-  dynamic "subnet" {
-    for_each = { for subnet in var.private_subnets : subnet.id => subnet }
-    content {
-      name        = "utility-${subnet.value.index + 1}"
-      type        = "Utility"
-      provider_id = subnet.value.id
-      zone        = subnet.value.zone
-    }
-  }
+#   dynamic "subnet" {
+#     for_each = { for subnet in var.private_subnets : subnet.id => subnet }
+#     content {
+#       name        = "utility-${subnet.value.index + 1}"
+#       type        = "Utility"
+#       provider_id = subnet.value.id
+#       zone        = subnet.value.zone
+#     }
+#   }
 
-  # etcd clusters
-  etcd_cluster {
-    name = "main"
-    member {
-      name           = "master-0"
-      instance_group = "master-0"
-    }
-    member {
-      name           = "master-1"
-      instance_group = "master-1"
-    }
-  }
-  etcd_cluster {
-    name = "events"
-    member {
-      name           = "master-0"
-      instance_group = "master-0"
-    }
-    member {
-      name           = "master-1"
-      instance_group = "master-1"
-    }
-  }
+#   # etcd clusters
+#   etcd_cluster {
+#     name = "main"
+#     member {
+#       name           = "master-0"
+#       instance_group = "master-0"
+#     }
+#     member {
+#       name           = "master-1"
+#       instance_group = "master-1"
+#     }
+#   }
+#   etcd_cluster {
+#     name = "events"
+#     member {
+#       name           = "master-0"
+#       instance_group = "master-0"
+#     }
+#     member {
+#       name           = "master-1"
+#       instance_group = "master-1"
+#     }
+#   }
 
-}
+# }
--- services/buckets.tf
+++ services/buckets.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "${local.subdomain}-cluster-state"
-
+  bucket        = "${local.subdomain}-cluster-state"
+  force_destroy = true
   tags = {
     Environment = "var.env_name"
   }
