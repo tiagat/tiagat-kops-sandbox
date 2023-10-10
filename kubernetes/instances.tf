9cec844976d1ae9564ac229384df168eaed2bd0e
Ruslan Sakal <ruslan.sakal@gmail.com>
2023-10-10 09:10:31 +0200

    fixed creation dependecies

--- kubernetes/instances.tf
+++ kubernetes/instances.tf
@@ -1,14 +1,14 @@
 resource "kops_instance_group" "master" {
 
-  for_each = { for subnet in var.private_subnets : subnet.id => subnet }
+  for_each = { for subnet in var.private_subnets : subnet.index => subnet }
 
   cluster_name = kops_cluster.cluster.name
-  name         = "master-${each.value.index + 1}"
+  name         = "master-${each.key + 1}"
   role         = "Master"
   min_size     = 1
   max_size     = 1
   machine_type = var.master_machine_type
-  subnets      = ["private-subnet-${each.value.index + 1}"]
+  subnets      = ["private-subnet-${each.key + 1}"]
 
   depends_on = [kops_cluster.cluster]
 
@@ -17,15 +17,15 @@ resource "kops_instance_group" "master" {
 
 resource "kops_instance_group" "node" {
 
-  for_each = { for subnet in var.private_subnets : subnet.id => subnet }
+  for_each = { for subnet in var.private_subnets : subnet.index => subnet }
 
   cluster_name = kops_cluster.cluster.name
-  name         = "node-${each.value.index + 1}"
+  name         = "node-${each.key + 1}"
   role         = "Node"
   min_size     = var.node_min_size
   max_size     = var.node_max_size
   machine_type = var.master_machine_type
-  subnets      = ["private-subnet-${each.value.index + 1}"]
+  subnets      = ["private-subnet-${each.key + 1}"]
 
   depends_on = [kops_cluster.cluster]
 
