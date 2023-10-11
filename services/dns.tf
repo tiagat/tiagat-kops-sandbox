5a1cdc439407a49e6c16bc5da3ddbebb4bb01d86
Ruslan Sakal <ruslan.sakal@gmail.com>
2023-10-11 11:11:57 +0200

    fixed dns record

--- services/dns.tf
+++ services/dns.tf
@@ -16,6 +16,16 @@ resource "aws_route53_record" "main-ns" {
   zone_id = data.aws_route53_zone.root.zone_id
   name    = local.subdomain
   type    = "NS"
-  ttl     = "86400"
+  ttl     = "300"
   records = aws_route53_zone.main.name_servers
+}
+
+resource "aws_route53_record" "main-a" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = local.subdomain
+  type    = "A"
+  ttl     = "300"
+  records = [
+    "127.0.0.1"
+  ]
 }
